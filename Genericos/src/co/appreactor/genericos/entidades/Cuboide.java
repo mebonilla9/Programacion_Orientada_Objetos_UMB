7431058ba8dfd9adb1b9868f0ccb7d2ae3fc927c
mebonilla9 <mebonilla9@gmail.com>
2018-10-17 17:35:29 -0500

    Correcciones menores

--- Genericos/src/co/appreactor/genericos/entidades/Cuboide.java
+++ Genericos/src/co/appreactor/genericos/entidades/Cuboide.java
@@ -10,7 +10,7 @@ package co.appreactor.genericos.entidades;
  * @author Lord_Nightmare
  */
 public class Cuboide<T> {
-    
+
     private T longitud;
     private T anchura;
     private T altura;
@@ -57,14 +57,28 @@ public class Cuboide<T> {
         this.altura = altura;
     }
 
+    public Double doubleValue(T valor) {
+        return Double.parseDouble(String.valueOf(valor));
+    }
+
+    public Double getVolumen() {
+        try {
+            return (Double) longitud * (Double) anchura * (Double) altura;
+        } catch (ClassCastException e) {
+            System.out.println("El valor generico de la clase cuboide no representa un dato numerico");
+            return 0.0;
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Longitud: ").append(longitud).append(" cm\n");
         sb.append("Anchura: ").append(anchura).append(" cm\n");
-        sb.append("Altura: ").append(altura).append(" cm");
+        sb.append("Altura: ").append(altura).append(" cm\n");
+        sb.append("--------------\n");
+        sb.append("Volumen: ").append(getVolumen()).append(" cm^3\n");
         return sb.toString();
     }
-    
-    
+
 }
